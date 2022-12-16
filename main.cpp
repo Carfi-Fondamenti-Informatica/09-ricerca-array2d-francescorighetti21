78005236f9ae445ce6912312545297167fe5aa8b
francescorighetti21 <117826738+francescorighetti21@users.noreply.github.com>
2022-12-17 00:11:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,44 +1,33 @@
 #include <iostream>
-
+#include "lib.h"
 using namespace std;
-int bodyguard (char lista[][20], char nome[20]){
-    int i=0, j=0;
-    while ((j<19)and(i<10)) {
-        if (lista[i][j] != nome[j]) {
-            i++;
-            j = 0;
-        } else if (lista[i][j] == nome[j]) {
-            j++;
-        }
-    } return 1;
-    }
+
 int main() {
     char lista[10][20];
-    char nome[20];
+    char target[20];
+    int pos = 0;
 
-    for (int x = 0; x < 10; x++) {
-        for (int y = 0; y < 20; y++) {
-            lista[x][y] = ' ';
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++) {
+            target[j] = ' ';
+            lista[i][j] = ' ';
         }
     }
-    for (int x = 0; x < 20; x++) {
-        nome[x] = ' ';
-    }
-    for (int i = 0; i < 10; ++i) {
+
+    for(int i=0;i<10;i++){
         cin >> lista[i];
     }
-    for (int x = 0; x < 10; x++) {
-        cout << lista[x] << endl;
-    }
-    cin >> nome;
 
-    int x = bodyguard (lista, nome);
-
-    if (x<10) {
-        cout << x;
-
-    } else if  (x == 10) {
+    cin >> target;
+    if(cerca(lista,pos,target)){
+        cout << pos;
+    }else{
         cout << "non presente";
     }
+
+
+
+
+
     return 0;
 }
